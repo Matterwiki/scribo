44207643ac9ae9158625ad4abaf0cdd9a7adfefd
Krishna Gopinath <gopinath@pointmanhq.com>
2018-09-03 14:31:44 -0400

    Restructure stuff for easy extras
    Progress: 40%

--- example/index.js
+++ example/index.js
@@ -8,15 +8,15 @@ import "./index.css";
 
 const RichTextEditorExample = () => (
     <div className="container">
-        <Editor placeholder="Start typing..." />
+        <Editor placeholder="Start typing..." dialogPortal="dialog-portal" />
     </div>
 );
 
 render(<RichTextEditorExample />, document.getElementById("root"));
 
-// TODO Make HMR work!
-if (module.hot) {
-    module.hot.dispose(() => {
-        window.location.reload();
-    });
-}
+// // TODO Make HMR work!
+// if (module.hot) {
+//     module.hot.dispose(() => {
+//         window.location.reload();
+//     });
+// }
