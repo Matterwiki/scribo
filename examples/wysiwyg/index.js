41f7bd49d80a3808fa34f50dff083d89f9d638c3
Krishna Gopinath <hpkrishna07@gmail.com>
2019-11-30 18:22:52 -0500

    [WIP] Ground zero
    Starting fresh instead of going through an insane migration process 🤷

--- examples/wysiwyg/index.js
+++ examples/wysiwyg/index.js
@@ -1,106 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-import { WYSIWYGEditor, fromJSON, toJSON } from '../../lib/wysiwyg/index';
-import './index.css';
+import { WYSIWYGEditor } from '../../lib/index'
 
-import { randomValue } from '../common/sample';
-
-function uploadFileHandler (file) {
-  console.log('File to upload:', file);
-
-  return new Promise((resolve, reject) => {
-    const reader = new window.FileReader();
-
-    reader.addEventListener('error', e => reject(e), false);
-    reader.addEventListener('load', () => resolve(reader.result), false);
-
-    // Timeout to simulate delay
-    setTimeout(() => {
-      if (file) reader.readAsDataURL(file);
-    }, 1000);
-  });
-}
-
-const Loading = ({ alt }) => <div>Uploading {alt}...</div>;
-const Error = ({ alt }) => <div>Error uploading {alt}...</div>;
-
-class RichTextEditorExample extends React.Component {
-  state = {
-    readOnly: false,
-    showPrintSink: false,
-    value: fromJSON(randomValue)
-  };
-
-  handleToggleReadOnly = () => {
-    this.setState(prevState => ({
-      readOnly: !prevState.readOnly
-    }));
-  };
-
-  handlePrintScriboState = () => {
-    console.log(toJSON(this.state.value));
-    this.setState({
-      showPrintSink: true
-    });
-  };
-
-  handleValueChanged = value => {
-    this.setState({ value });
-  };
-
-  render () {
-    const imageUploadOptions = {
-      Loading,
-      Error,
-      uploadFileHandler
-    };
-
-    const { value, showPrintSink } = this.state;
-
-    return (
-      <>
-        <h2>
-          <code>WYSIWYGEditor</code> component
-        </h2>
-        <div className='container'>
-          <WYSIWYGEditor
-            readOnly={this.state.readOnly}
-            value={value}
-            onValueChanged={this.handleValueChanged}
-            placeholder='Start typing...'
-            imageUploadOptions={imageUploadOptions}
-          />
-        </div>
-        <div>
-          <input
-            type='button'
-            value='Toggle `readOnly` mode'
-            onClick={this.handleToggleReadOnly}
-          />
-          <input
-            type='button'
-            value='Print value'
-            onClick={this.handlePrintScriboState}
-          />
-        </div>
-        {showPrintSink && (
-          <div>
-            <pre>
-              <code>{JSON.stringify(toJSON(value), null, 2)}</code>
-            </pre>
-          </div>
-        )}
-      </>
-    );
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'A line of text in a paragraph.',
+        marks: []
+      }
+    ]
   }
-}
+]
 
-render(<RichTextEditorExample />, document.getElementById('root'));
+const RichTextEditorExample = () => (
+  <WYSIWYGEditor
+    editorDefaultValue={initialValue}
+    buttonActiveColor='limegreen'
+    buttonDefaultColor='#393e41'
+    buttonIconSize={15}
+  />
+)
 
-// // TODO Make HMR work!
+render(<RichTextEditorExample />, document.getElementById('root'))
+
+// Only works for dev mode
+// https://parceljs.org/hmr.html
 // if (module.hot) {
-//     module.hot.dispose(() => {
-//         window.location.reload();
-//     });
+//   module.hot.accept()
 // }
